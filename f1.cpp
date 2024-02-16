317aa0fa3a563ad3e494998980777c308e9e8460
Prit Patel <patelprit985@gmail.com>
2024-02-16 09:51:48 -0500

    Added the final code of the assignment

--- f1.cpp
+++ f1.cpp
@@ -2,7 +2,17 @@
 
 int main(void)
 {
-	
+	int number = 9;
+
+	/* print the number upon startup */
+	printf("Here is all the numbers!");
+	printf("The number is %d\n", number);
+
+	/* change the value of the number and
+	 * print it */
+	number = 8;
+	printf("The number is now %d, not %d\n",
+		number, 5);
 
 	return 0;
 }
